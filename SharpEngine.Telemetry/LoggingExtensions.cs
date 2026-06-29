0723ef99e7513d139ff1ef24fa46748035a59e32
Antti Veikkolainen <antti.veikkolainen00@outlook.com>
2026-06-29 20:35:02 +0300

    Add logging

---
+++ SharpEngine.Telemetry/LoggingExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SharpEngine.Telemetry;
+
+/// <summary>
+///     Contains extensions for logging messages.
+/// </summary>
+public static class LoggingExtensions
+{
+    /// <summary>
+    ///     Creates a logger with console output for the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type for which to create the logger.</typeparam>
+    /// <param name="logLevel">The minimum log level for the logger.</param>
+    /// <returns>A logger instance configured with console logging.</returns>
+    public static ILogger<T> CreateLogger<T>(LogLevel logLevel = LogLevel.Information)
+        => LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(logLevel)).CreateLogger<T>();
+
+    /// <summary>
+    ///     Creates a logger with console output for the specified type.
+    /// </summary>
+    /// <param name="type">The type for which to create the logger.</param>
+    /// <param name="logLevel">The minimum log level for the logger.</param>
+    /// <returns>A logger instance configured with console logging.</returns>
+    public static ILogger CreateLogger(Type type, LogLevel logLevel = LogLevel.Information)
+        => LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(logLevel)).CreateLogger(type);
+}
