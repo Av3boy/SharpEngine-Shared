c1945849ec31cef2b6ee2682ae50229134f22b87
Antti Veikkolainen <antti.veikkolainen00@outlook.com>
2026-09-21 00:53:27 +0300

    Move content

---
+++ SharpEngine.IO/Extensions/PathExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace SharpEngine.IO.Extensions;
+
+/// <summary>
+///     Contains extensions for handling paths and files.
+/// </summary>
+public static class PathExtensions
+{
+    /// <summary>
+    ///     Gets the path for the file in the executing assembly directory.
+    /// </summary>
+    /// <param name="file">The file whose path is being resolved.</param>
+    /// <returns>The file in the currently executing assembly context.</returns>
+    public static string GetAssemblyPath(string file)
+        => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file);
+}
--- SharpEngine.Shared/Math.cs
+++ SharpEngine.Shared/Math.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using SharpEngine.Core.Numerics;
 
 namespace SharpEngine.Core;
